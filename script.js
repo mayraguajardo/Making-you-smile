a6b8e0ffbfba093a7f0dceac9a827b82b958309d
Mayra Guajardo <A01634475@itesm.mx>
2021-04-08 02:08:27 -0500

    making yu smile
    Signed-off-by: Mayra Guajardo <A01634475@itesm.mx>

---
+++ script.js
@@ -0,0 +1,48 @@
+function generarHora(){
+   var now = new Date()
+   var h = now.getHours()
+   var m = now.getMinutes()
+   var s = now.getSeconds()
+   h = agregarCeros(h)
+   m = agregarCeros(m)
+   s = agregarCeros(s)
+   //Buscar nuestro reloj en nuestro html
+   document.getElementById("reloj").innerText = h + ':' + m + ':' + s
+   var t = setTimeout(generarHora, 500);
+}
+
+function agregarCeros(tiempo){
+   if(tiempo < 10){
+       tiempo = "0"+tiempo
+   }
+   return tiempo
+}
+
+function revisarQuiz(){
+    let count = 0
+
+    var edadElement = document.getElementById("edad")
+    var edad = edadElement.options[edadElement.selectedIndex].value
+    
+    var comidaElement = document.getElementById("comida")
+    var comida = comidaElement.options[comidaElement.selectedIndex].value
+
+    var lenguajeElement = document.getElementById("lenguaje")
+    var lenguaje = lenguajeElement.options[lenguajeElement.selectedIndex].value
+
+    if(edad === "21"){
+        count++
+    }
+
+    if(comida === "Pizza"){
+        count++
+    }
+
+    if(lenguaje === "Python"){
+        count++
+    }
+
+    alert("Tuviste: " + count + "/3 respuestas correctas.")
+}
+
+generarHora()
